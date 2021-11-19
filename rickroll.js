9b8830d4815147c51b08db23c641d143a7ad7c8f
Sami Jakonen <sami.jakonen@iki.fi>
2021-11-19 12:39:04 +0200

    Don't use static domain in the server

--- rickroll.js
+++ rickroll.js
@@ -3,22 +3,22 @@ header-rickroll
 sjaks@github.com
 */
 
-var http = require('http');
-var fs = require('fs');
+let http = require('http');
+let fs = require('fs');
 
 // Load movie file and init frames,
 // the height of one frame is 12 lines in the .txt file
-var movie = fs.readFileSync("movie.txt").toString().split("\n");
-var frame = 0;
-var inc = 12;
-var attributes = "ABCDEFGHIJKL";
+let movie = fs.readFileSync("movie.txt").toString().split("\n");
+let frame = 0;
+let inc = 12;
+let attributes = "ABCDEFGHIJKL";
 
 
 http.createServer(function (req, res) {
     if (req.headers['user-agent'].indexOf("curl") < 0) {
         // If the requester is not cURL, prompt the visitor to access this
         // app via cURL. --max-redirs -1 allows infinite redirects
-        res.write("$ curl --max-redirs -1 -IL https://sjaks.dy.fi/headers");
+        res.write("$ curl --max-redirs -1 -IL " + req.headers.host + req.url);
         res.end();
         return;
     }
@@ -28,7 +28,7 @@ http.createServer(function (req, res) {
         frame = 0;
 
     // Format the animation into  HTTP headers
-    for (var i = 0; i < inc; i++) {
+    for (let i = 0; i < inc; i++) {
         let headerAttribute = "X-" + attributes.charAt(i);
         res.setHeader(headerAttribute, "'" +  movie[frame + i].replace(/\r?\n|\r/g, " ") + "'");
     }
@@ -37,7 +37,7 @@ http.createServer(function (req, res) {
 
     setTimeout(function() {
         res.writeHead(301, {
-            Location: "https://sjaks.dy.fi/headers"
+            Location: req.url
         });
 
         res.end();
